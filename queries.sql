f3e8ab214a3723b9549fa3a9ca9a8d41a0bf81e3
milhousecvh <135559562+milhousecvh@users.noreply.github.com>
2023-12-02 15:08:40 -0600

    forgot to push SQL

--- queries.sql
+++ queries.sql
@@ -8,7 +8,16 @@ string name
 string skills
 
 --Part 2
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 --Part 3
+DROP TABLE job;
 
 --Part 4
+SELECT *
+FROM skills
+INNER JOIN job_skills ON skills.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
